d00a08313f6dad7935d27250cef9c1a6d73a8f60
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2016-02-29 22:52:06 -0500

    sisesat-main.R

---
+++ sisesat-main.R
@@ -0,0 +1,22 @@
+# # crea las carpetas
+# crearFolder <- function(directorio, nombre, ...){
+#   salida <- .createFolder(directorio, nombre,...)
+#   return(salida)
+# }
+# 
+# # genera la base para cada barco 2000-2009
+# idBarco  <- function(directorio, anho, caletas, ...){ 
+#   salida <- .baseBoat(directorio, anho, caletas, ... ) 
+#   return(salida) 
+# } 
+# 
+# # genera la base para cada barco 2010-
+# idBarco2 <- function(directorio, anho, caletas, ...){ 
+#   salida <- .baseBoat2(directorio, anho, caletas, ... ) 
+#   return(salida) 
+# } 
+# # Reconocimiento del viaje
+# idViaje <- function(directorio, anho, ...){
+#   salida <- .cacularViaje(directorio, anho, ...)
+#   return(salida)
+# }
