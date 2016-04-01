03bd13407b0d854ec92a39babbf482401d3d72cf
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2016-04-01 11:44:19 -0500

    lol

--- R/sisesat-bitacora.R
+++ R/sisesat-bitacora.R
@@ -1,4 +1,10 @@
 
 # Cruce de la informacion SISESAT - BITACORA ------------------------------
 
+readDataSat <- function(data, ...){
+  # 
+}
 
+linkSatBit <- function(dataSat, dataBit, ...){
+  #
+}
