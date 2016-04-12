54d88750425d94781c244eb2631abd540f7b0587
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2016-04-12 10:06:19 -0500

    change rumbo

--- Demo/Demo.R
+++ Demo/Demo.R
@@ -9,16 +9,16 @@ require(shape)    # Arrows (mapVMS)
 
 rm(data)
 directorio = "E:/vms_prueba_tesis/data_vms"
-#directorio = "E:/enviar_IMARPE_data2010a2015"
-#readVMSvesselafter(directorio, year = 2000:2014, caletas = caletas)
 
-#allPuertos = read.csv("E:/package/base_puertos.csv")
-#temporadaSardina = read.csv("E:/package/temporada_sardina.csv")
-# 
+# directorio = "E:/enviar_IMARPE_data2010a2015"
+# readVMSvesselafter(directorio, year = 2000:2014, caletas = caletas)
+
+# allPuertos = read.csv("E:/package/base_puertos.csv")
+# temporadaSardina = read.csv("E:/package/temporada_sardina.csv")
+
 # save(allPuertos, file = "allPuertos.rda")
 # save(temporadaSardina, file = "temporadaSardina.rda")
 # vamos a probar el readVMSvesselbefore2010
 
-
-temporadaAnchovetaNC = read.csv("E:/package/temporada_anch_nc.csv")
-save(temporadaAnchovetaNC, file = "temporadaAnchovetaNC.rda")                
+# temporadaAnchovetaNC = read.csv("E:/package/temporada_anch_nc.csv")
+# save(temporadaAnchovetaNC, file = "temporadaAnchovetaNC.rda")                
